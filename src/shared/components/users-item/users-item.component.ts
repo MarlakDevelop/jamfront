3f612ccb13438ee8b07d2c4e5d39fb888cd4c1d0
Marlak's Computer <mik.myk2015@gmail.com>
2021-04-13 07:38:12 +0400

    Fixing socket glitches V3

--- src/shared/components/users-item/users-item.component.ts
+++ src/shared/components/users-item/users-item.component.ts
@@ -1,35 +1,47 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { UserAction } from '@models/user-action.model';
-import {SocketService} from '@services/socket.service';
+import { SocketService } from '@services/socket.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-users-item',
   templateUrl: './users-item.component.html',
   styleUrls: ['./users-item.component.sass']
 })
-export class UsersItemComponent implements OnInit{
+export class UsersItemComponent implements OnInit, OnDestroy {
   @Input() userActions: UserAction[];
   @Input() id: number;
   @Input() username: string;
   @Input() image: string;
   @Input() online: boolean;
+  onUserOnlineSocketSub: Subscription;
+  onUserOfflineSocketSub: Subscription;
 
   constructor(
     private socketService: SocketService
   ) {}
 
   ngOnInit() {
-    this.socketService.onUserOnline(this.id).subscribe(
+    this.onUserOnlineSocketSub = this.socketService.onUserOnline(this.id).subscribe(
       res => {
         this.online = true;
       },
       err => {}
     );
-    this.socketService.onUserOffline(this.id).subscribe(
+    this.onUserOfflineSocketSub = this.socketService.onUserOffline(this.id).subscribe(
       res => {
         this.online = false;
       },
       err => {}
     );
+  }
+
+  ngOnDestroy() {
+    if (this.onUserOnlineSocketSub){
+      this.onUserOnlineSocketSub.unsubscribe();
+    }
+    if (this.onUserOfflineSocketSub){
+      this.onUserOfflineSocketSub.unsubscribe();
+    }
   }
 }
