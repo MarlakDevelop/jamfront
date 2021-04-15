ac22e1568e7a1fc606abb29c664eb79f0c941be8
Marlak's Computer <mik.myk2015@gmail.com>
2021-04-15 18:59:24 +0400

    Fixing sockets duplicate

--- src/pages/main/messenger/chat/chat.component.ts
+++ src/pages/main/messenger/chat/chat.component.ts
@@ -46,41 +46,15 @@ export class ChatComponent implements OnInit, OnDestroy {
     private dialog: MatDialog,
     private socketService: SocketService
   ) {
-    this.onChatNewMessageSocketSub = this.socketService.onChatNewMessage(+this.route.snapshot.paramMap.get('id')).subscribe(
-      res => {
-        this.messages = this.messages.reverse();
-        this.messages.push({
-          id: res.data.message.id,
-          username: res.data.message.author.user.username,
-          image: res.data.message.author.user.image,
-          text: res.data.message.text,
-          date: res.data.message.date_created
-        });
-        this.messages = this.messages.reverse();
-        this.messagesBoxComponent.updateFlexBox();
-      },
-      err => {
-      }
-    );
-    this.onChatMembersUpdateSocketSub = this.socketService.onChatMembersUpdate(+this.route.snapshot.paramMap.get('id')).subscribe(
-      res1 => {
-        this.aSub = this.chatsService.getMembers(+this.route.snapshot.paramMap.get('id')).subscribe(
-          res => {
-            this.members = res.members.map(member => member.member.member.user);
-          },
-          err => this.router.navigateByUrl('/messenger')
-        );
-      },
-      err1 => {
-      }
-    );
     this.navSub = this.router.events.subscribe((e: any) => {
       if (e instanceof NavigationEnd) {
         if (this.onChatNewMessageSocketSub){
           this.onChatNewMessageSocketSub.unsubscribe();
+          this.onChatNewMessageSocketSub = null;
         }
         if (this.onChatMembersUpdateSocketSub){
           this.onChatMembersUpdateSocketSub.unsubscribe();
+          this.onChatNewMessageSocketSub = null;
         }
         this.getChat();
         this.messagesConfig = {
@@ -94,43 +68,45 @@ export class ChatComponent implements OnInit, OnDestroy {
         this.loadMessages();
         this.members = [];
         this.getMembers();
-        if (!this.onChatNewMessageSocketSub) {
-          this.onChatNewMessageSocketSub = this.socketService.onChatNewMessage(+this.route.snapshot.paramMap.get('id')).subscribe(
-            res => {
-              this.messages = this.messages.reverse();
-              this.messages.push({
-                id: res.data.message.id,
-                username: res.data.message.author.user.username,
-                image: res.data.message.author.user.image,
-                text: res.data.message.text,
-                date: res.data.message.date_created
-              });
-              this.messages = this.messages.reverse();
-              this.messagesBoxComponent.updateFlexBox();
-            },
-            err => {
-            }
-          );
-        }
-        if (!this.onChatMembersUpdateSocketSub) {
-          this.onChatMembersUpdateSocketSub = this.socketService.onChatMembersUpdate(+this.route.snapshot.paramMap.get('id')).subscribe(
-            res1 => {
-              this.aSub = this.chatsService.getMembers(+this.route.snapshot.paramMap.get('id')).subscribe(
-                res => {
-                  this.members = res.members.map(member => member.member.member.user);
-                },
-                err => this.router.navigateByUrl('/messenger')
-              );
-            },
-            err1 => {
-            }
-          );
-        }
+        this.onChatNewMessageSocketSub = this.socketService.onChatNewMessage(+this.route.snapshot.paramMap.get('id')).subscribe(
+          res => {
+            this.messages = this.messages.reverse();
+            this.messages.push({
+              id: res.data.message.id,
+              username: res.data.message.author.user.username,
+              image: res.data.message.author.user.image,
+              text: res.data.message.text,
+              date: res.data.message.date_created
+            });
+            this.messages = this.messages.reverse();
+            this.messagesBoxComponent.updateFlexBox();
+          },
+          err => {}
+        );
+        this.onChatMembersUpdateSocketSub = this.socketService.onChatMembersUpdate(+this.route.snapshot.paramMap.get('id')).subscribe(
+          res1 => {
+            this.aSub = this.chatsService.getMembers(+this.route.snapshot.paramMap.get('id')).subscribe(
+              res => {
+                this.members = res.members.map(member => member.member.member.user);
+              },
+              err => this.router.navigateByUrl('/messenger')
+            );
+          },
+          err1 => {}
+        );
       }
     });
   }
 
   ngOnInit() {
+    if (this.onChatNewMessageSocketSub){
+      this.onChatNewMessageSocketSub.unsubscribe();
+      this.onChatNewMessageSocketSub = null;
+    }
+    if (this.onChatMembersUpdateSocketSub){
+      this.onChatMembersUpdateSocketSub.unsubscribe();
+      this.onChatNewMessageSocketSub = null;
+    }
     this.onChatNewMessageSocketSub = this.socketService.onChatNewMessage(+this.route.snapshot.paramMap.get('id')).subscribe(
       res => {
         this.messages = this.messages.reverse();
